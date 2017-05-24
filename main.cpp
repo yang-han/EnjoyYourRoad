8b0bd0c72042a7bcf7536d2990e305e9c16f6e62
YogHurt <yanghan9826@gmail.com>
2017-05-24 14:20:45 +0800

    *

--- main.cpp
+++ main.cpp
@@ -61,8 +61,6 @@ int main( void )
         // Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
         glClear( GL_COLOR_BUFFER_BIT );
 
-        // Draw nothing, see you in tutorial 2 !
-
 
         // Swap buffers
         glfwSwapBuffers(window);
