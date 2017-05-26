23517c71356b71c16048c89f891aab8e59c98773
YogHurt <yanghan9826@gmail.com>
2017-05-26 10:52:20 +0800

    triagle

--- main.cpp
+++ main.cpp
@@ -13,6 +13,10 @@
 #include <glfw3.h>
 GLFWwindow* window;
 
+
+
+#include <common/shader.hpp>
+
 // Include GLM
 #include <glm/glm.hpp>
 using namespace glm;
@@ -34,7 +38,7 @@ int main( void )
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Open a window and create its OpenGL context
-    window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
+    window = glfwCreateWindow( 1024, 768, "Road", NULL, NULL);
     if( window == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
         getchar();
@@ -44,6 +48,7 @@ int main( void )
     glfwMakeContextCurrent(window);
 
     // Initialize GLEW
+    glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK) {
         fprintf(stderr, "Failed to initialize GLEW\n");
         getchar();
@@ -57,11 +62,37 @@ int main( void )
     // Dark blue background
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+
+    GLuint programID = LoadShaders("VertexShader.vs", "FragmentShader.fs");
+
+    static const GLfloat vertex_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, 1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+    };
+
+    GLuint VertexBuffer;
+    glGenBuffers(1, &VertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data, GL_STATIC_DRAW);
+
+
     do{
         // Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
         glClear( GL_COLOR_BUFFER_BIT );
 
-        // Draw nothing, see you in tutorial 2 !
+
+        glUseProgram(programID);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
+        glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE,0, (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
 
 
         // Swap buffers
@@ -71,6 +102,12 @@ int main( void )
     } // Check if the ESC key was pressed or the window was closed
     while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
            glfwWindowShouldClose(window) == 0 );
+
+
+    glDeleteBuffers(1, &VertexBuffer);
+    glDeleteVertexArrays(1, &VertexArrayID);
+    glDeleteProgram(programID);
+
 
     // Close OpenGL window and terminate GLFW
     glfwTerminate();
