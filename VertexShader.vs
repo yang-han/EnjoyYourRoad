f50aab77871c74e5e4c1650f7c820d6b3ef3f44e
YogHurt <yanghan9826@gmail.com>
2017-05-28 20:13:50 +0800

    begin

--- VertexShader.vs
+++ VertexShader.vs
@@ -1,7 +1,9 @@
 #version 330 core
-layout(location = 0) in vec3 vertexPosition;
+layout(location = 0) in vec3 vertexPosition_modelspace;
+
+uniform mat4 MVP;
 
 void main(){
-    gl_Position.xyz = vertexPosition;
-    gl_Position.w = 1.0;
+    gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
+
 }
--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ GLFWwindow* window;
 
 // Include GLM
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
 int main( void )
@@ -67,36 +68,73 @@ int main( void )
     glBindVertexArray(VertexArrayID);
 
 
-    GLuint programID = LoadShaders("VertexShader.vs", "FragmentShader.fs");
+    GLuint programID = LoadShaders("../VertexShader.vs", "../FragmentShader.fs");
+
+    // Get a handle for our "MVP" uniform
+    GLint MatrixID = glGetUniformLocation(programID, "MVP");
+
+    // Projection matrix : 45?Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+    // Or, for an ortho camera :
+    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+    // Camera matrix
+    glm::mat4 View       = glm::lookAt(
+            glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+            glm::vec3(0,0,0), // and looks at the origin
+            glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+    );
+    // Model matrix : an identity matrix (model will be at the origin)
+    glm::mat4 Model      = glm::mat4(1.0f);
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
 
     static const GLfloat vertex_data[] = {
-            -1.0f, -1.0f, 0.0f,
             1.0f, 1.0f, 0.0f,
-            0.0f, 1.0f, 0.0f,
+            -1.0f, 0.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+//            1.0f, -1.0f, -1.0f,
+//            -1.0f, 1.0f, 1.0f,
+//            -1.0f, 1.0f, -1.0f,
+//            -1.0f, -1.0f, 1.0f,
+//            -1.0f, -1.0f, -1.0f,
     };
 
-    GLuint VertexBuffer;
-    glGenBuffers(1, &VertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data, GL_STATIC_DRAW);
 
 
     do{
-        // Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+
+        // Clear the screen
         glClear( GL_COLOR_BUFFER_BIT );
 
-<<<<<<< HEAD
-
+        // Use our shader
         glUseProgram(programID);
 
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
-        glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE,0, (void*)0);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDisableVertexAttribArray(0);
+        // Send our transformation to the currently bound shader,
+        // in the "MVP" uniform
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
-=======
->>>>>>> 8b0bd0c72042a7bcf7536d2990e305e9c16f6e62
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+                0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+                3,                  // size
+                GL_FLOAT,           // type
+                GL_FALSE,           // normalized?
+                0,                  // stride
+                (void*)0            // array buffer offset
+        );
+
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+        glDisableVertexAttribArray(0);
 
         // Swap buffers
         glfwSwapBuffers(window);
@@ -106,8 +144,7 @@ int main( void )
     while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
            glfwWindowShouldClose(window) == 0 );
 
-
-    glDeleteBuffers(1, &VertexBuffer);
+    glDeleteBuffers(1, &vertexbuffer);
     glDeleteVertexArrays(1, &VertexArrayID);
     glDeleteProgram(programID);
 
